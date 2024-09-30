04ba32bc177aa51fef40b34f21e6015047cc3d55
LobbyLobster <humblerogue33@gmail.com>
2024-09-30 17:10:24 +0300

    rabbitmq initial

--- main.go
+++ main.go
@@ -1,130 +1,136 @@
 package main
 
 import (
-    "github.com/monobilisim/monokit/common"
-    "github.com/monobilisim/monokit/osHealth"
-    "github.com/spf13/cobra"
-    "fmt"
-    "os"
+	"fmt"
+	"github.com/monobilisim/monokit/common"
+	"github.com/monobilisim/monokit/osHealth"
+	"github.com/monobilisim/monokit/rabbitmq"
+	"github.com/spf13/cobra"
+	"os"
 )
 
 var MonokitVersion = "devel"
 var RootCmd = &cobra.Command{
-    Use: "monokit",
-    Version: MonokitVersion,
+	Use:     "monokit",
+	Version: MonokitVersion,
 }
 
 func main() {
-    var osHealthCmd = &cobra.Command{
-        Use:   "osHealth",
-        Short: "OS Health",
-        Run: osHealth.Main,
-    }
-   
-    //// Common
-    RootCmd.AddCommand(common.RedmineCmd)
-    RootCmd.AddCommand(common.AlarmCmd)
+	var osHealthCmd = &cobra.Command{
+		Use:   "osHealth",
+		Short: "OS Health",
+		Run:   osHealth.Main,
+	}
 
-    /// Alarm
-    
-    // AlarmSend
-    common.AlarmCmd.AddCommand(common.AlarmSendCmd)
+	//// Common
+	RootCmd.AddCommand(common.RedmineCmd)
+	RootCmd.AddCommand(common.AlarmCmd)
 
-    common.AlarmSendCmd.Flags().StringP("message", "m", "", "Message")
-    common.AlarmSendCmd.MarkFlagRequired("message")
+	/// Alarm
 
-    // AlarmCheckUp
-    common.AlarmCmd.AddCommand(common.AlarmCheckUpCmd)
+	// AlarmSend
+	common.AlarmCmd.AddCommand(common.AlarmSendCmd)
 
-    common.AlarmCheckUpCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.AlarmCheckUpCmd.Flags().StringP("message", "m", "", "Message")
-    common.AlarmCheckUpCmd.Flags().StringP("scriptName", "n", "", "Script name")
-    common.AlarmCheckUpCmd.MarkFlagRequired("message")
-    common.AlarmCheckUpCmd.MarkFlagRequired("service")
-    common.AlarmCheckUpCmd.MarkFlagRequired("scriptName")
+	common.AlarmSendCmd.Flags().StringP("message", "m", "", "Message")
+	common.AlarmSendCmd.MarkFlagRequired("message")
 
-    // AlarmCheckDown
-    common.AlarmCmd.AddCommand(common.AlarmCheckDownCmd)
+	// AlarmCheckUp
+	common.AlarmCmd.AddCommand(common.AlarmCheckUpCmd)
 
-    common.AlarmCheckDownCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.AlarmCheckDownCmd.Flags().StringP("message", "m", "", "Message")
-    common.AlarmCheckDownCmd.Flags().StringP("scriptName", "n", "", "Script name")
-    common.AlarmCheckDownCmd.MarkFlagRequired("message")
-    common.AlarmCheckDownCmd.MarkFlagRequired("service")
-    common.AlarmCheckDownCmd.MarkFlagRequired("scriptName")
+	common.AlarmCheckUpCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.AlarmCheckUpCmd.Flags().StringP("message", "m", "", "Message")
+	common.AlarmCheckUpCmd.Flags().StringP("scriptName", "n", "", "Script name")
+	common.AlarmCheckUpCmd.MarkFlagRequired("message")
+	common.AlarmCheckUpCmd.MarkFlagRequired("service")
+	common.AlarmCheckUpCmd.MarkFlagRequired("scriptName")
 
+	// AlarmCheckDown
+	common.AlarmCmd.AddCommand(common.AlarmCheckDownCmd)
 
-    /// Redmine
-    common.RedmineCmd.AddCommand(common.RedmineIssueCmd)
-    
-    // RedmineCreate
-    common.RedmineIssueCmd.AddCommand(common.RedmineCreateCmd)
-    
-    common.RedmineCreateCmd.Flags().StringP("subject", "j", "", "Subject")
-    common.RedmineCreateCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineCreateCmd.Flags().StringP("message", "m", "", "Message")
-    common.RedmineCreateCmd.MarkFlagRequired("subject")
-    common.RedmineCreateCmd.MarkFlagRequired("service")
-    common.RedmineCreateCmd.MarkFlagRequired("message")
+	common.AlarmCheckDownCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.AlarmCheckDownCmd.Flags().StringP("message", "m", "", "Message")
+	common.AlarmCheckDownCmd.Flags().StringP("scriptName", "n", "", "Script name")
+	common.AlarmCheckDownCmd.MarkFlagRequired("message")
+	common.AlarmCheckDownCmd.MarkFlagRequired("service")
+	common.AlarmCheckDownCmd.MarkFlagRequired("scriptName")
 
-    // RedmineUpdate
-    common.RedmineIssueCmd.AddCommand(common.RedmineUpdateCmd)
-    
-    common.RedmineUpdateCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineUpdateCmd.Flags().StringP("message", "m", "", "Message")
-    common.RedmineUpdateCmd.Flags().BoolP("checkNote", "c", false, "Check Notes")
-    common.RedmineUpdateCmd.MarkFlagRequired("service")
-    common.RedmineUpdateCmd.MarkFlagRequired("message")
-    
-    // RedmineClose
-    common.RedmineIssueCmd.AddCommand(common.RedmineCloseCmd)
-    
-    common.RedmineCloseCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineCloseCmd.Flags().StringP("message", "m", "", "Message")
-    common.RedmineCloseCmd.MarkFlagRequired("service")
-    common.RedmineCloseCmd.MarkFlagRequired("message")
+	/// Redmine
+	common.RedmineCmd.AddCommand(common.RedmineIssueCmd)
 
-    // RedmineShow
-    common.RedmineIssueCmd.AddCommand(common.RedmineShowCmd)
-    
-    common.RedmineShowCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineShowCmd.MarkFlagRequired("service")
+	// RedmineCreate
+	common.RedmineIssueCmd.AddCommand(common.RedmineCreateCmd)
 
-    // RedmineExists
-    common.RedmineIssueCmd.AddCommand(common.RedmineExistsCmd)
+	common.RedmineCreateCmd.Flags().StringP("subject", "j", "", "Subject")
+	common.RedmineCreateCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineCreateCmd.Flags().StringP("message", "m", "", "Message")
+	common.RedmineCreateCmd.MarkFlagRequired("subject")
+	common.RedmineCreateCmd.MarkFlagRequired("service")
+	common.RedmineCreateCmd.MarkFlagRequired("message")
 
-    common.RedmineExistsCmd.Flags().StringP("subject", "j", "", "Subject")
-    common.RedmineExistsCmd.Flags().StringP("date", "d", "", "Date")
-    common.RedmineExistsCmd.Flags().BoolP("search", "s", false, "Search")
+	// RedmineUpdate
+	common.RedmineIssueCmd.AddCommand(common.RedmineUpdateCmd)
 
-    common.RedmineExistsCmd.MarkFlagRequired("subject")
+	common.RedmineUpdateCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineUpdateCmd.Flags().StringP("message", "m", "", "Message")
+	common.RedmineUpdateCmd.Flags().BoolP("checkNote", "c", false, "Check Notes")
+	common.RedmineUpdateCmd.MarkFlagRequired("service")
+	common.RedmineUpdateCmd.MarkFlagRequired("message")
 
-    // RedmineCheckUp
-    common.RedmineIssueCmd.AddCommand(common.RedmineCheckUpCmd)
+	// RedmineClose
+	common.RedmineIssueCmd.AddCommand(common.RedmineCloseCmd)
 
-    common.RedmineCheckUpCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineCheckUpCmd.Flags().StringP("message", "m", "", "Message")
+	common.RedmineCloseCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineCloseCmd.Flags().StringP("message", "m", "", "Message")
+	common.RedmineCloseCmd.MarkFlagRequired("service")
+	common.RedmineCloseCmd.MarkFlagRequired("message")
 
-    common.RedmineCheckUpCmd.MarkFlagRequired("service")
-    common.RedmineCheckUpCmd.MarkFlagRequired("message")
+	// RedmineShow
+	common.RedmineIssueCmd.AddCommand(common.RedmineShowCmd)
 
-    // RedmineCheckDown
-    common.RedmineIssueCmd.AddCommand(common.RedmineCheckDownCmd)
+	common.RedmineShowCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineShowCmd.MarkFlagRequired("service")
 
-    common.RedmineCheckDownCmd.Flags().StringP("service", "s", "", "Service Name")
-    common.RedmineCheckDownCmd.Flags().StringP("message", "m", "", "Message")
-    common.RedmineCheckDownCmd.Flags().StringP("subject", "j", "", "Subject")
-    common.RedmineCheckDownCmd.MarkFlagRequired("subject")
-    common.RedmineCheckDownCmd.MarkFlagRequired("service")
-    common.RedmineCheckDownCmd.MarkFlagRequired("message")
+	// RedmineExists
+	common.RedmineIssueCmd.AddCommand(common.RedmineExistsCmd)
 
-    /// OS Health
-    RootCmd.AddCommand(osHealthCmd)
+	common.RedmineExistsCmd.Flags().StringP("subject", "j", "", "Subject")
+	common.RedmineExistsCmd.Flags().StringP("date", "d", "", "Date")
+	common.RedmineExistsCmd.Flags().BoolP("search", "s", false, "Search")
 
-    RedisCommandAdd()
-    
-    if err := RootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	common.RedmineExistsCmd.MarkFlagRequired("subject")
+
+	// RedmineCheckUp
+	common.RedmineIssueCmd.AddCommand(common.RedmineCheckUpCmd)
+
+	common.RedmineCheckUpCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineCheckUpCmd.Flags().StringP("message", "m", "", "Message")
+
+	common.RedmineCheckUpCmd.MarkFlagRequired("service")
+	common.RedmineCheckUpCmd.MarkFlagRequired("message")
+
+	// RedmineCheckDown
+	common.RedmineIssueCmd.AddCommand(common.RedmineCheckDownCmd)
+
+	common.RedmineCheckDownCmd.Flags().StringP("service", "s", "", "Service Name")
+	common.RedmineCheckDownCmd.Flags().StringP("message", "m", "", "Message")
+	common.RedmineCheckDownCmd.Flags().StringP("subject", "j", "", "Subject")
+	common.RedmineCheckDownCmd.MarkFlagRequired("subject")
+	common.RedmineCheckDownCmd.MarkFlagRequired("service")
+	common.RedmineCheckDownCmd.MarkFlagRequired("message")
+
+	/// OS Health
+	RootCmd.AddCommand(osHealthCmd)
+
+	RedisCommandAdd()
+
+	RootCmd.AddCommand(&cobra.Command{
+		Use:   "rabbitmqHealth",
+		Short: "RabbitMQ Health",
+		Run:   rabbitmq.Main,
+	})
+
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
