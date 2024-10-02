fa62319b83db557eb96b3754a182f9de3150779a
Kreato (Work) <kreato@kreato.dev>
2024-10-02 21:15:19 +0300

    tests/redmine/issue/create: also use show

--- tests/redmine/issue/create.sh
+++ tests/redmine/issue/create.sh
@@ -4,5 +4,8 @@ set -e
 # Create a new project
 ./bin/monokit redmine issue create --message test --service test --subject test
 
+# Get issue ID
+./bin/monokit redmine issue show --service test
+
 # Check if project was created
 ./bin/monokit redmine issue exists --subject test
