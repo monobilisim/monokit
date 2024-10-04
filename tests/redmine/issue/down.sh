6f5439cbfd81c67a3b97ad22134d8b61b6897dce
Kreato (Work) <kreato@kreato.dev>
2024-10-05 00:50:56 +0300

    fix issue/down test

--- tests/redmine/issue/down.sh
+++ tests/redmine/issue/down.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Create a new issue using down cmd
-./bin/monokit redmine issue down --message test --service test --subject test_updown
+./bin/monokit redmine issue down --message test --service test_updown --subject test_updown
 
 # Wait for the interval to end
 sleep 60
 
 # Try to create issue again as the interval has ended
-./bin/monokit redmine issue down --message test --service test --subject test_updown
+./bin/monokit redmine issue down --message test --service test_updown --subject test_updown
 
 # Get issue ID
-./bin/monokit redmine issue show --service test
+./bin/monokit redmine issue show --service test_updown
 
 # Check if issue was created
 ./bin/monokit redmine issue exists --subject test_updown
