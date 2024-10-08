2e2b3a2b63df0380ba61032059cd1c469d0facb6
Kreato (Work) <kreato@kreato.dev>
2024-10-08 12:12:28 +0300

    mysqlHealth: add initial command

---
+++ mysqlHealth/main.go
@@ -0,0 +1,22 @@
+package mysqlHealth
+
+import (
+    "fmt"
+    "time"
+    "github.com/spf13/cobra"
+    "github.com/monobilisim/monokit/common"
+    db "github.com/monobilisim/monokit/common/db"
+)
+
+var DbHealthConfig db.DbHealth
+
+func Main(cmd *cobra.Command, args []string) {
+    version := "3.0.0"
+    common.ScriptName = "mysqlHealth"
+    common.TmpDir = common.TmpDir + "mysqlHealth"
+    common.Init()
+    common.ConfInit("db", &DbHealthConfig)
+
+    fmt.Println("MySQL Health Check REWRITE - v" + version + " - " + time.Now().Format("2006-01-02 15:04:05"))
+    
+}
