897e3b3ad277c430512e5c925807da8df2cd4d62
Bekir Pehlivan <bekir.pehlivan@mono.net.tr>
2024-10-25 17:49:23 +0300

    active connections

--- common/db/main.go
+++ common/db/main.go
@@ -1,32 +1,31 @@
 package common
 
 type Mysql struct {
-    Process_limit int
-    Cluster struct {
-        Enabled bool
-        Size int
-        Check_table_day string
-        Check_table_hour string
-    }
-    Alarm struct {
-        Enabled bool
-    }
+	Process_limit int
+	Cluster       struct {
+		Enabled          bool
+		Size             int
+		Check_table_day  string
+		Check_table_hour string
+	}
+	Alarm struct {
+		Enabled bool
+	}
 }
 
 type Postgres struct {
-    Limit struct {
-        Process int
-        Query int
-        Conn_percent int
-    }
-    Alarm struct {
-        Enabled bool
-    }
-    Leader_switch_hook string
+	Limits struct {
+		Process      int
+		Query        int
+		Conn_percent int
+	}
+	Alarm struct {
+		Enabled bool
+	}
+	Leader_switch_hook string
 }
 
-
 type DbHealth struct {
-    Mysql Mysql
-    Postgres Postgres
+	Mysql    Mysql
+	Postgres Postgres
 }
