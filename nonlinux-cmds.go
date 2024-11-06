5954b6f074d571129e9d026bc77627898ccc1a2a
LobbyLobster <humblerogue33@gmail.com>
2024-11-06 20:08:47 +0300

    mysqlHealth nonlinux

--- nonlinux-cmds.go
+++ nonlinux-cmds.go
@@ -1,22 +1,28 @@
 //go:build !linux
+
 package main
 
 func RedisCommandAdd() {
-    // redisHealth is not supported on anything other than Linux
-    return
+	// redisHealth is not supported on anything other than Linux
+	return
+}
+
+func MysqlCommandAdd() {
+	// mysqlHealth is not supported on anything other than Linux
+	return
 }
 
 func RmqCommandAdd() {
-    // rmqHealth is not supported on anything other than Linux
-    return
+	// rmqHealth is not supported on anything other than Linux
+	return
 }
 
 func PmgCommandAdd() {
-    // pmgHealth is not supported on anything other than Linux
-    return
+	// pmgHealth is not supported on anything other than Linux
+	return
 }
 
 func PostalCommandAdd() {
-    // postalHealth is not supported on anything other than Linux
-    return
+	// postalHealth is not supported on anything other than Linux
+	return
 }
