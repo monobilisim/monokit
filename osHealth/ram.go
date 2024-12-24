ba75aba358c60cb6d471c9fb0ea78cc68524befa
Kreato <kreato@kreato.dev>
2024-12-24 10:26:09 +0300

    osHealth: improve grammar on Redmine issue title

--- osHealth/ram.go
+++ osHealth/ram.go
@@ -21,11 +21,11 @@ func RamUsage() {
     if virtualMemory.UsedPercent > ramLimit {
         common.PrettyPrint("RAM Usage", common.Fail + " more than " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%", virtualMemory.UsedPercent, true, false, false, 0)
         common.AlarmCheckDown("ram", "RAM usage limit has exceeded " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "% (Current: " + strconv.FormatFloat(virtualMemory.UsedPercent, 'f', 0, 64) + "%)", false)
-        issues.CheckDown("ram", common.Config.Identifier + " için hafıza kullanımı " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%'nin üstüne çıktı", "Hafıza kullanımı: " + strconv.FormatFloat(virtualMemory.UsedPercent, 'f', 0, 64) + "%\n Hafıza limiti: " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%", false, 0)
+        issues.CheckDown("ram", common.Config.Identifier + " için hafıza kullanımı %" + strconv.FormatFloat(ramLimit, 'f', 0, 64) + " üstüne çıktı", "Hafıza kullanımı: " + strconv.FormatFloat(virtualMemory.UsedPercent, 'f', 0, 64) + "%\n Hafıza limiti: " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%", false, 0)
     } else {
         common.PrettyPrint("RAM Usage", common.Green + " less than " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%", virtualMemory.UsedPercent, true, false, false, 0)
         common.AlarmCheckUp("ram", "RAM usage went below " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "% (Current: " + strconv.FormatFloat(virtualMemory.UsedPercent, 'f', 0, 64) + "%)", false)
-        issues.CheckUp("ram", common.Config.Identifier + " için hafıza kullanımı " + strconv.FormatFloat(ramLimit, 'f', 0, 64) + "%'nin altına düştü")
+        issues.CheckUp("ram", common.Config.Identifier + " için hafıza kullanımı %" + strconv.FormatFloat(ramLimit, 'f', 0, 64) + " altına düştü")
     }
 }
 
