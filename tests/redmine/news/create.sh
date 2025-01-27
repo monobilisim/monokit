169265ed44519c18df3fa12f8be794cbb4cf092d
Kreato (Work) <kreato@kreato.dev>
2025-01-27 13:38:36 +0300

    tests/redmine/news/create: add sleep between creating and checking the news

--- tests/redmine/news/create.sh
+++ tests/redmine/news/create.sh
@@ -4,5 +4,7 @@ set -e
 # Create new news
 ./bin/monokit redmine news create --description test --title test
 
+sleep 5 # Wait for news to be created
+
 # Check if news were created
 ./bin/monokit redmine news exists --title test --description test > /tmp/news_id # Save news id to file to use in other tests
