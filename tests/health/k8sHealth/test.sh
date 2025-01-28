ea130052a64d497bdae2324041976294e316e653
Kreato <kreato@kreato.dev>
2025-01-28 15:00:41 +0300

    ci: rmqHealth/k8sHealth: add greps to check output

--- tests/health/k8sHealth/test.sh
+++ tests/health/k8sHealth/test.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+set -e
+
 sudo mkdir -p /etc/mono
 sudo touch /etc/mono/k8s.yaml
-./bin/monokit k8sHealth
+export MONOKIT_NOCOLOR=1
+
+./bin/monokit k8sHealth > out.log
+
+cat out.log | grep "is Ready"
+cat out.log | grep "rke2-ingress-nginx-config.yaml is not available"
+cat out.log | grep "the server could not find the requested resource"
+cat out.log | grep "kube-vip is not available"
+cat out.log | grep "serving-kube-apiserver.crt is not available"
--- tests/health/rmqHealth/test.sh
+++ tests/health/rmqHealth/test.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-./bin/monokit rmqHealth
+set -e
+
+export MONOKIT_NOCOLOR=1
+./bin/monokit rmqHealth > out.log
+cat out.log | grep "rabbitmq-server is active"
+cat out.log | grep "Port 5672 is active"
+
+cat out.log | grep "Overview is not reachable"
+cat out.log | grep "Node list is not reachable"
