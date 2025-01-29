08d1fe1f3e123523d7bf566efd228533685aa278
Kreato (Work) <kreato@kreato.dev>
2025-01-29 10:13:33 +0300

    tests/health/pgsqlHealth: add debug output

--- tests/health/pgsqlHealth/test.sh
+++ tests/health/pgsqlHealth/test.sh
@@ -8,6 +8,10 @@ sudo cp ./config/db.yml /etc/mono/db.yml
 
 sudo -u postgres ./bin/monokit pgsqlHealth > out.log
 
+echo "=== Output ==="
+cat out.log
+echo "=== End of Output ==="
+
 grep -q "PostgreSQL is accessible" out.log
 grep -q "PostgreSQL uptime is 0d" out.log
 grep -q "Number of active connections is 6/100" out.log
