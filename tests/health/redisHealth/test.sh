a78b32ebd66f3f1c99d10891dfe720485d1d9bcb
Kreato (Work) <kreato@kreato.dev>
2025-01-29 11:34:56 +0300

    tests/health/redisHealth: finish

--- tests/health/redisHealth/test.sh
+++ tests/health/redisHealth/test.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
-MONOKIT_NOCOLOR=1 ./bin/monokit redisHealth
+MONOKIT_NOCOLOR=1 ./bin/monokit redisHealth > out.log
+
+grep -q "Redis is pingable" out.log
+grep -q "Role is master" out.log
+grep -q "Service redis-server is active" out.log
+grep -q "Redis is writeable" out.log
+grep -q "Redis is readable" out.log
