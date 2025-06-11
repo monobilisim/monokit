0bdc9f62009bb3d7c49f5a26bb145ef761e43cd4
Kreato <kreato@kreato.dev>
2025-06-11 12:24:49 +0300

    Disable syncConfigFn call in SendReq function

--- common/api/client.go
+++ common/api/client.go
@@ -230,7 +230,7 @@ var (
 
 func SendReq(apiVersion string) {
 	// Sync host configuration with server
-	syncConfigFn(nil, nil)
+	//syncConfigFn(nil, nil)
 
 	beforeHost, err := getReqFn(apiVersion)
 
